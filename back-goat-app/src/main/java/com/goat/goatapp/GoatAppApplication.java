b86784a7f327cf3782ed4d7372cf7d50c7798663
leehaneul623 <dyj03216@naver.com>
2022-09-29 10:27:13 +0900

    팀 리포지터리 생성

---
+++ back-goat-app/src/main/java/com/goat/goatapp/GoatAppApplication.java
@@ -0,0 +1,13 @@
+package com.goat.goatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoatAppApplication.class, args);
+	}
+
+}
---
+++ back-goat-app/src/test/java/com/goat/goatapp/GoatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goat.goatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoatAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
